1cf2bf715659e176eb6f6ff71f9e4aad1d2fc2b7
Amitdhale <amitdhale88@gmail.com>
2024-02-02 16:12:09 +0530

    few changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "88e0fe84294f2526"; }
+/******/ 		__webpack_require__.h = function() { return "b95eb8af6c27db5d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
